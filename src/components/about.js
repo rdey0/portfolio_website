2fe9c76c6ad91857bd474fc6f7db1d82af780d65
Roy Dey <roydey10@gmail.com>
2023-02-27 20:31:59 -0800

    update resume and about

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import me from '../images/me.png'
-import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 import Reveal from 'react-reveal/Reveal';
 import SectionTitle from './section_title'
 
 export default function About() {
-    
-    return(
+
+    return (
         <div id='about-container' className='section'>
-            <SectionTitle text='ABOUT' justify='center'/>
+            <SectionTitle text='ABOUT' justify='center' />
             <Reveal effect='fade-slide-up'>
                 <div id='about-header-container' className='hidden'>
                     <p className='short-description'>
@@ -22,21 +22,21 @@ export default function About() {
                     <div id='about-body-container'>
                         <Reveal effect='fade-slide-right'>
                             <div className='hidden'>
-                                <img id='profile-pic' src={me}/>
+                                <img id='profile-pic' src={me} />
                             </div>
                         </Reveal>
                         <Reveal effect='fade-slide-left'>
                             <div id='about-body' className='hidden'>
                                 <div id='about-body-title'> Hi There</div>
                                 <p id='body-long-description'>
-                                    I'm a UC Davis computer science graduate with an interest in full stack development and
-                                    artificial intelligence. When I'm not working on my passion projects, I enjoy weightlifting, 
-                                    biking, and staying up to date on the latest hip hop releases.
-                                    <br/>
-                                    <br/>
+                                    I'm a full stack engineer with experience working the freight forwarding industry to deliver clean UIs and robust server-side logic.
+                                    When I'm not working on my passion projects, I enjoy weightlifting,
+                                    running, and staying up to date on the latest hip hop releases.
+                                    <br />
+                                    <br />
                                     I'm currently searching for a software development position so if
                                     my portfolio piques your interest and you're hiring, give me a shout.
-                                    
+
                                 </p>
                                 <div id='about-my-info'>
                                     <div className='about-info-item'>
@@ -54,18 +54,18 @@ export default function About() {
                                     <div className='about-info-item'>
                                         <div className='about-info-title'>Socials:</div>
                                         <div className='about-info-content'>
-                                            <a 
-                                            href={'https://github.com/rdey0'}
-                                            target='_blank' 
-                                            rel='noopener noreferrer' >
-                                                <AiFillGithub className='about-social-icon'/>
+                                            <a
+                                                href={'https://github.com/rdey0'}
+                                                target='_blank'
+                                                rel='noopener noreferrer' >
+                                                <AiFillGithub className='about-social-icon' />
                                             </a>
-                                            
-                                            <a 
-                                            href={'https://www.linkedin.com/in/roydey/'}
-                                            target='_blank' 
-                                            rel='noopener noreferrer' >
-                                                <AiFillLinkedin className='about-social-icon'/>
+
+                                            <a
+                                                href={'https://www.linkedin.com/in/roydey/'}
+                                                target='_blank'
+                                                rel='noopener noreferrer' >
+                                                <AiFillLinkedin className='about-social-icon' />
                                             </a>
                                         </div>
                                     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import resume from '../resume/Roy Dey - Resume.pdf'
+import resume from '../resume/Roy Dey Resume.pdf'
 //Hide navbar when scrolling down and show when scrolling up
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar-container").style.top = "0";
-  } else {
-    document.getElementById("navbar-container").style.top = "-5em";
-  }
-  prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar-container").style.top = "0";
+    } else {
+        document.getElementById("navbar-container").style.top = "-5em";
+    }
+    prevScrollpos = currentScrollPos;
 }
 
-export default class NavBar extends React.Component{
+export default class NavBar extends React.Component {
     // Scroll to whatever section was clicked on
-    handle_click=(event)=> {
+    handle_click = (event) => {
         var selected_section = event.target.innerText;
         var section_id = selected_section.toLowerCase() + '-container';
         var element = document.getElementById(section_id);
@@ -24,15 +24,15 @@ export default class NavBar extends React.Component{
         });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div id='navbar-container-wrapper'>
                 <div id='navbar-container'>
                     <div className='item-container no-interaction'>
                         <div className='navbar-item' onClick={this.handle_click}>HOME</div>
                         <a href={resume}
-                            target='_blank' 
-                            rel='noopener noreferrer'className='navbar-item'>RESUME</a>
+                            target='_blank'
+                            rel='noopener noreferrer' className='navbar-item'>RESUME</a>
                         <div className='navbar-item' onClick={this.handle_click}>ABOUT</div>
                         <div className='navbar-item' onClick={this.handle_click}>SKILLS</div>
                         <div className='navbar-item' onClick={this.handle_click}>PORTFOLIO</div>
@@ -40,7 +40,7 @@ export default class NavBar extends React.Component{
                     </div>
                 </div>
             </div>
-            
+
         )
-    }  
+    }
 }
